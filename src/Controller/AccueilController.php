cad2e6c94c7bcf160a0c850586640edaac58270b
Deblox <deblox.160999@gmail.com>
2023-06-06 11:27:13 +0200

    change accueil => page

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -11,42 +11,42 @@ class AccueilController extends AbstractController
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
-        return $this->render('accueil/accueil.html.twig', [
+        return $this->render('page/accueil.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
     #[Route('/categorie', name: 'app_cat')]
     public function cat(): Response
     {
-        return $this->render('accueil/categorie.html.twig', [
+        return $this->render('page/categorie.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
     #[Route('/plat', name: 'app_plat')]
     public function plat(): Response
     {
-        return $this->render('accueil/plat.html.twig', [
+        return $this->render('page/plat.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
     #[Route('/contact', name: 'app_contact')]
     public function contact(): Response
     {
-        return $this->render('accueil/contact.html.twig', [
+        return $this->render('page/contact.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
         #[Route('/mentionslegales', name: 'app_mlegal')]
     public function mlegal(): Response
     {
-        return $this->render('accueil/mlegal.html.twig', [
+        return $this->render('page/mlegal.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
     #[Route('/cgu', name: 'app_cgu')]
     public function cgu(): Response
     {
-        return $this->render('accueil/cgu.html.twig', [
+        return $this->render('page/cgu.html.twig', [
             'controller_name' => 'AccueilController',
         ]);
     }
---
+++ var/cache/dev/twig/2f/2f19d8dc8f57d35a99b266613c3db390.php
@@ -0,0 +1,226 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* page/contact.html.twig */
+class __TwigTemplate_b8a86723030d40c941431173e8d715a3 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'stylesheets' => [$this, 'block_stylesheets'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "page/contact.html.twig"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "page/contact.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "page/contact.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        echo "Contact";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 5
+    public function block_stylesheets($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
+
+        // line 6
+        echo "<link rel=\"stylesheet\" href=\"";
+        echo "src/Css/contact.css";
+        echo "\">";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    // line 8
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 9
+        echo "
+
+
+<div class='row my-5 align-items-center'>
+    <form action=\"#\" method='POST'>
+        <div class=\"input-group \">
+            <div class=\"row mx-auto my-5\">
+
+                        <div class=\"input-group-append col\">
+                            <label for=\"nomcontact\" class=\"contactlabel\">Nom :</label>
+                            <input type=\"text\" name=\"nomcontact\" id=\"nomcontact\" class=\"contactinput\">
+                        </div>
+
+                        <div class=\"input-group-append col\">
+                            <label for=\"prenomcontact\" class=\"contactlabel\">Prenom :</label>
+                            <input type=\"text\" name=\"prenomcontact\" id=\"prenomcontact\" class=\"contactinput\">
+                        </div>
+
+                        <div class=\"input-group-append col\">
+                            <label for=\"mailcontact\" class=\"contactlabel\">Mail :</label>
+                            <input type=\"text\" name=\"mailcontact\" id=\"mailcontact\" class=\"contactinput\">
+                        </div>
+                        
+                        <div class='row my-5'>
+                            <label for=\"demandecontact\" class=\"contactlabel\">Demande :</label>
+                            <input type=\"text\" name=\"demandecontact\" id=\"demandecontact\" class=\"contactinput demandecontact\">
+                        </div>
+                        <div class=\"col-6\"></div>
+                        <div class='col'>
+                        <input type=\"submit\" class='btn btn-primary'>
+                        </div>
+
+            </div>
+        </div>
+    </form>
+  </div>
+
+
+
+";
+        
+        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
+
+        
+        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "page/contact.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  111 => 9,  101 => 8,  89 => 6,  79 => 5,  60 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+
+{% block title %}Contact{% endblock %}
+
+{% block stylesheets %}
+<link rel=\"stylesheet\" href=\"{{('src/Css/contact.css')}}\">{% endblock %}
+
+{% block body %}
+
+
+
+<div class='row my-5 align-items-center'>
+    <form action=\"#\" method='POST'>
+        <div class=\"input-group \">
+            <div class=\"row mx-auto my-5\">
+
+                        <div class=\"input-group-append col\">
+                            <label for=\"nomcontact\" class=\"contactlabel\">Nom :</label>
+                            <input type=\"text\" name=\"nomcontact\" id=\"nomcontact\" class=\"contactinput\">
+                        </div>
+
+                        <div class=\"input-group-append col\">
+                            <label for=\"prenomcontact\" class=\"contactlabel\">Prenom :</label>
+                            <input type=\"text\" name=\"prenomcontact\" id=\"prenomcontact\" class=\"contactinput\">
+                        </div>
+
+                        <div class=\"input-group-append col\">
+                            <label for=\"mailcontact\" class=\"contactlabel\">Mail :</label>
+                            <input type=\"text\" name=\"mailcontact\" id=\"mailcontact\" class=\"contactinput\">
+                        </div>
+                        
+                        <div class='row my-5'>
+                            <label for=\"demandecontact\" class=\"contactlabel\">Demande :</label>
+                            <input type=\"text\" name=\"demandecontact\" id=\"demandecontact\" class=\"contactinput demandecontact\">
+                        </div>
+                        <div class=\"col-6\"></div>
+                        <div class='col'>
+                        <input type=\"submit\" class='btn btn-primary'>
+                        </div>
+
+            </div>
+        </div>
+    </form>
+  </div>
+
+
+
+{% endblock %}
+", "page/contact.html.twig", "/home/antoine/Bureau/FormatioAfpa/the_district_sym/templates/page/contact.html.twig");
+    }
+}
