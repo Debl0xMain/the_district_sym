ecdaf0044f7ec26b7b6521ef2870aff271af2748
Debl0xMain <deblox.160999@gmail.com>
2023-07-05 04:59:45 +0200

    add

--- public/src/js/panier.js
+++ public/src/js/panier.js
@@ -102,75 +102,73 @@ $(".addpanier").on('click',function(e){
  }
 
 const writepanier = () => {
-                            if (document.getElementById(`panierrow${id}`)) {
+                            
+                            
+    console.log("tableau panier php ="+panier);
 
-                                qtevalue = document.getElementById(`qteselect${id}`).value;
-                                qtevalue++;
+    var id = panier.id;
+   console.log("id ="+id);
 
-                                const ajaxadd = (qtevalue) => {
-                                    $.ajax({
-                                        type: "POST",
-                                        url: "/panierqte",
-                                        data: {qtevalue: qtevalue,idplat:id},
-                                        success: function(data){
-                                            console.log("qte add"+data);
-                                    }})
-                                }
+    var qtevalue = panier.qte;
+    console.log('qte = '+qtevalue);
 
-                                if(qtevalue == '2'){
-                                    $(`#qteselect${id} option[value="2"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                }
-                                if(qtevalue == '3'){
-                                    $(`#qteselect${id} option[value="3"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                }
-                                if(qtevalue == '4'){
-                                    $(`#qteselect${id} option[value="4"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                } 
-                                if(qtevalue == '5'){
-                                    $(`#qteselect${id} option[value="5"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                }
-                                if(qtevalue == '6'){
-                                    $(`#qteselect${id} option[value="6"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                } 
-                                if(qtevalue == '7'){
-                                    $(`#qteselect${id} option[value="7"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                } 
-                                if(qtevalue == '8'){
-                                    $(`#qteselect${id} option[value="8"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                } 
-                                if(qtevalue == '9'){
-                                    $(`#qteselect${id} option[value="9"]`).prop('selected', true);
-                                    ajaxadd(qtevalue);
-                                } 
-                        }
+    var prix = panier.prix;
+    console.log("prix = "+prix);
 
-                    else {
+    var img = panier.img;
+    console.log("img = "+img);
+    var image = panier.image;
+    console.log("image = "+image);
 
-                         console.log("ajout panier"+data["libelle"]);
-                         
-                         $('.panierbody').before(`<div class="row rows-col-4 panierrow${id}" id='panierrow${id}'></div>`);
+    var panierjs = panier;
+    console.log("tableau panier js ="+panierjs);
 
-                         $('.panierrow'+id).append(`<div class="col my-auto imgpaniercol${id}"></div>`);
-                         $('.imgpaniercol'+id).append(`<img class="panierimg" src="src/img/plat/${data['image']}" alt="${data['image']}" height="32px" width="32px">`);
-                         
-                         $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierlibelle${id}"></div>`);
-                         $('.panierlibelle'+id).append(`${data['libelle']}`);
+    var libelle = panier.libelle;
+    console.log("libelle = "+libelle);
+/*
+        console.log("ajout panier ajoute depuis le panier session"+libelle);
+        
+        $('.panierbody').before(`<div class="row rows-col-4 panierrow${id}" id='panierrow${id}'></div>`);
 
-                         $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierprix${id}"></div>`);
-                         $('.panierprix'+id).append(`${data['prix']}`);
+        $('.panierrow'+id).append(`<div class="col my-auto imgpaniercol${id}"></div>`);
+        $('.imgpaniercol'+id).append(`<img class="panierimg" src="src/img/plat/${image}" alt="${image}" height="32px" width="32px">`);
+        
+        $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierlibelle${id}"></div>`);
+        $('.panierlibelle'+id).append(`${libelle}`);
 
-                         $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierqte${id}"></div>`);
-                         $('.panierqte'+id).append(`<select name="qteselect" id="qteselect${id}"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select>`);
+        $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierprix${id}"></div>`);
+        $('.panierprix'+id).append(`${prix}`);
 
-                         $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierpaniersup${id}"></div>`);
-                        $('.panierpaniersup'+id).append(`<button onclick="dlton(${id})" type="submit" class="btn deletepanierbtn" id='deletepanierbtn' data-id="${id}"><i class="fa-solid fa-trash"></i></button>`);
-                    }
+        $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierqte${id}"></div>`);
+        $('.panierqte'+id).append(`<select name="qteselect" id="qteselect${id}"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option></select>`);
+
+        $('.panierrow'+id).append(`<div class="col my-auto col my-auto panierpaniersup${id}"></div>`);
+       $('.panierpaniersup'+id).append(`<button onclick="dlton(${id})" type="submit" class="btn deletepanierbtn" id='deletepanierbtn' data-id="${id}"><i class="fa-solid fa-trash"></i></button>`);
+   
+               if(qtevalue == '2'){
+                   $(`#qteselect${id} option[value="2"]`).prop('selected', true);
+               }
+               if(qtevalue == '3'){
+                   $(`#qteselect${id} option[value="3"]`).prop('selected', true);  
+               }
+               if(qtevalue == '4'){
+                   $(`#qteselect${id} option[value="4"]`).prop('selected', true);   
+               } 
+               if(qtevalue == '5'){
+                   $(`#qteselect${id} option[value="5"]`).prop('selected', true);
+               }
+               if(qtevalue == '6'){
+                   $(`#qteselect${id} option[value="6"]`).prop('selected', true);
+               } 
+               if(qtevalue == '7'){
+                   $(`#qteselect${id} option[value="7"]`).prop('selected', true);
+               } 
+               if(qtevalue == '8'){
+                   $(`#qteselect${id} option[value="8"]`).prop('selected', true);
+               } 
+               if(qtevalue == '9'){
+                   $(`#qteselect${id} option[value="9"]`).prop('selected', true);
+               } 
+          */                  
 
 }
